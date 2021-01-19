e82bfbf0db27dca822df46a4321f5c3cbb9c83ec
HarryNdegwa <harrisonndegwa65@gmail.com>
2021-01-20 00:27:38 +0300

    Added register simple form

--- App.js
+++ App.js
@@ -13,8 +13,8 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Login" component={LoginScreen} />
         <Stack.Screen name="Register" component={RegisterScreen} />
+        <Stack.Screen name="Login" component={LoginScreen} />
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Edit" component={EditScreen} />
       </Stack.Navigator>
--- src/screens/RegisterScreen.js
+++ src/screens/RegisterScreen.js
@@ -1,10 +1,54 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, Button, TextInput } from "react-native";
+import { Formik } from "formik";
 
 function RegisterScreen(props) {
   return (
     <View>
       <Text>Register</Text>
+      <Formik
+        initialValues={{
+          name: "",
+          email: "",
+          phone: "",
+          role: "",
+          profile: "",
+          password: "",
+        }}
+        onSubmit={(values) => console.log(values)}
+      >
+        {({ handleChange, handleBlur, handleSubmit, values }) => (
+          <View>
+            <Text>Name</Text>
+            <TextInput
+              onChangeText={handleChange("name")}
+              onBlur={handleBlur("name")}
+              value={values.name}
+            />
+            <Text>Email</Text>
+            <TextInput
+              onChangeText={handleChange("email")}
+              onBlur={handleBlur("email")}
+              value={values.email}
+            />
+            <Text>Phone</Text>
+            <TextInput
+              onChangeText={handleChange("phone")}
+              onBlur={handleBlur("phone")}
+              value={values.phone}
+            />
+            <Text>Role</Text>
+
+            <Text>Password</Text>
+            <TextInput
+              onChangeText={handleChange("password")}
+              onBlur={handleBlur("password")}
+              value={values.password}
+            />
+            <Button onPress={handleSubmit} title="Register" />
+          </View>
+        )}
+      </Formik>
     </View>
   );
 }
