4c2be7ac0c222d7f38cec43abba8eddf23681912
HarryNdegwa <harrisonndegwa65@gmail.com>
2021-01-20 02:28:29 +0300

    Added profile markup in home screen

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -5,6 +5,11 @@ function HomeScreen(props) {
   return (
     <View>
       <Text>Home</Text>
+      <Text>Name:Mike</Text>
+      <Text>Email:test@gmail.com</Text>
+      <Text>Phone:01081297383</Text>
+      <Text>Role:ADMIN</Text>
+      <Text>Profile Image:.....</Text>
     </View>
   );
 }
