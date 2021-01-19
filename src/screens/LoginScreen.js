fce1fe8193c6ba9475a6be3ccdc5101fd13f7694
HarryNdegwa <harrisonndegwa65@gmail.com>
2021-01-19 23:52:00 +0300

    Added login form

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,11 +1,33 @@
 import React from "react";
-import { View, Text, TextInput } from "react-native";
+import { View, Text, TextInput, Button } from "react-native";
 import { Formik } from "formik";
 
 function LoginScreen(props) {
   return (
     <View>
       <Text>Login</Text>
+      <Formik
+        initialValues={{ primary: "", password: "" }}
+        onSubmit={(values) => console.log(values)}
+      >
+        {({ handleChange, handleBlur, handleSubmit, values }) => (
+          <View>
+            <Text>Email/Phone</Text>
+            <TextInput
+              onChangeText={handleChange("primary")}
+              onBlur={handleBlur("primary")}
+              value={values.primary}
+            />
+            <Text>Password</Text>
+            <TextInput
+              onChangeText={handleChange("password")}
+              onBlur={handleBlur("password")}
+              value={values.password}
+            />
+            <Button onPress={handleSubmit} title="Login" />
+          </View>
+        )}
+      </Formik>
     </View>
   );
 }
