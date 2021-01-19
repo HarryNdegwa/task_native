c85b09f65d7b2902cfcea4e8578a9acc73fc341f
HarryNdegwa <harrisonndegwa65@gmail.com>
2021-01-19 23:40:47 +0300

    Added formik package

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, TextInput } from "react-native";
+import { Formik } from "formik";
 
 function LoginScreen(props) {
   return (
